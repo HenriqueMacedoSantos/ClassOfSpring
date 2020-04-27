fd5f566f6efedc9b1cfaec5ff0cf3140123083ff
henrique macedo <henriquemacedo2016s@gmail.com>
2020-04-26 21:40:02 -0300

    second commit

---
+++ ProjeFinal/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into wizards(GWR, name, house, age, patron, animal, wand)
+            values (1, 'Henrique', 'Slytherin', 10, 'Bunny', 'owl', 'holly, 28cm, phoenix feather, good and malleable');
+
+insert into wizards(GWR, name, house, age, patron, animal, wand)
+            values (2, 'Dani', 'Slytherin', 10, 'Fox', 'Cat', 'vine, with 27.30 cm and dragon heart fiber core');
+
+insert into wizards(GWR, name, house, age, patron, animal, wand)
+            values (3, 'Renata', 'Hufflepuff', 10, 'Otter', 'owl', 'Inflexible, 24 cm, made of Rosewood and Veela hair core');
